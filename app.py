9556ada051e204237d4d2c6da79e00dd1e311865
Abhishek-1310 <kumaryadavabhi1999@gmail.com>
2023-08-10 20:14:22 +0530

    modify

--- app.py
+++ app.py
@@ -1,10 +1,66 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+@app.route("/",methods=['GET','POST'])
+def home_page():
+    return render_template('index.html')
+
+@app.route("/math", methods=['POST'])
+def math_ops():
+    if(request.method=='POST'):
+        ops=request.form['operation']
+        num1=int(request.form['num1'])
+        num2=int(request.form['num2'])
+        if(ops=='add'):
+            r=num1+num2
+            res='The sum of  '+str(num1)+' and '+str(num2)+' is:-'+str(r)
+            return render_template('result.html',result=res)
+        if(ops=='subtract'):
+            r=num1-num2
+            res='The subtract of  '+str(num1)+' and '+str(num2)+' is:-'+str(r)
+            return render_template('result.html',result=res)
+        if(ops=='multiply'):
+            r=num1*num2
+            res='The multiply of  '+str(num1)+' and '+str(num2)+' is:-'+str(r)
+            return render_template('result.html',result=res)
+        if(ops=='divide'):
+            r=num1/num2
+            res='The divide of  '+str(num1)+' and '+str(num2)+' is:-'+str(r)
+            return render_template('result.html',result=res)
+        if(ops=='log'):
+            r=num1%num2
+            res='The reminder of  '+str(num1)+' and '+str(num2)+' is:-'+str(r)
+            return render_template('result.html',result=res)
+
+@app.route("/postman_data", methods=['POST'])
+def math_ops1():
+    if(request.method=='POST'):
+        ops=request.json['operation']
+        num1=int(request.json['num1'])
+        num2=int(request.json['num2'])
+        if(ops=='add'):
+            r=num1+num2
+            res='The sum of  '+str(num1)+' and '+str(num2)+' is:-'+str(r)
+            
+        if(ops=='subtract'):
+            r=num1-num2
+            res='The subtract of  '+str(num1)+' and '+str(num2)+' is:-'+str(r)
+            
+        if(ops=='multiply'):
+            r=num1*num2
+            res='The multiply of  '+str(num1)+' and '+str(num2)+' is:-'+str(r)
+            
+        if(ops=='divide'):
+            r=num1/num2
+            res='The divide of  '+str(num1)+' and '+str(num2)+' is:-'+str(r)
+            
+        if(ops=='log'):
+            r=num1%num2
+            res='The reminder of  '+str(num1)+' and '+str(num2)+' is:-'+str(r)
+        
+        return jsonify(res)
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
